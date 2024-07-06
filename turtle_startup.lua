003f7e6f15fa36c2ac75efdcd14edd2a46efa8a6
ahauter <hauterame.com>
2024-07-06 15:36:58 -0700

    Rewrite of turtle logic

--- turtle_startup.lua
+++ turtle_startup.lua
@@ -7,57 +7,80 @@ local nav = require("navigation")
 local protocol = require("mine_protocol")
 local quarry = require("quarry")
 
-local version = "0.0.1"
+local version = "0.0.2"
+
+local status = nil
+local quarry_location = nil
 
 local function receive_message()
-    local id, mess = rednet.receive(protocol.name, 1)
-    if mess ~= nil then
-        rednet.broadcast("New message: " .. mess, "monitor")
-    end
-    if mess ~= nil and string.find(mess, "^" .. protocol.headers.spot_assignment) ~= nil then
-        return "quarry", mess
-    elseif dump.need_dump() then
-        return "dump", ""
-    elseif fuel.need_refuel() then
-        return "refuel", ""
-    elseif mess ~= nil and string.find(mess, "^" .. protocol.headers.recall) ~= nil then
-        return "go_home", ""
-    else
-        return "new spot"
+    while true do
+        -- set a timeout so we keep checking for
+        local id, mess = rednet.receive(1)
+        if mess ~= nil then
+            rednet.broadcast("New message: " .. mess, "monitor")
+            if string.find(mess, "^" .. protocol.headers.spot_assignment) ~= nil then
+                local spot, y = protocol.parse_spot_assignment(mess)
+                quarry_location = {
+                    spot = spot,
+                    y = y
+                }
+            elseif string.find(mess, "^" .. protocol.headers.status_report) ~= nil then
+                rednet.send(id, protocol.headers.status_report .. " " .. status)
+            elseif string.find(mess, "^" .. protocol.headers.recall) ~= nil then
+                status = "go_home"
+            elseif string.find(mess, "^" .. protocol.headers.restart) ~= nil then
+                status = "restart"
+            end
+        end
+        if dump.need_dump() then
+            status = "dump"
+        elseif fuel.need_refuel() then
+            status = "refuel"
+        elseif mess == nil then
+            status = "mine"
+        end
     end
 end
-local current_spot = nil
+
+local quary_coroutine = nil
 local function run_mine()
-    print("Turtle starting up! Version " .. version)
-    rednet.open("left")
-    rednet.broadcast("Turtle starting up!", "monitor")
     while true do
-        local command, res = receive_message()
-        rednet.broadcast("Current Status: " .. command, "monitor")
-        if command == "quarry" then
-            local spot, y = protocol.parse_spot_assignment(res)
-            current_spot = spot
-            local need_breack = quarry.quarry_level(spot, y)
-            if not need_breack then
-                protocol.send_new_level(spot)
-            end
-            y = y - 1
-            protocol.leave_spot(spot)
-            current_spot = nil
-        elseif command == "go_home" then
+        if status == "go_home" then
             nav.goto_block(location.home)
-        elseif command == "dump" then
+        elseif status == "dump" then
             nav.goto_block(dump.location)
             dir.face(dir.NEG_Z)
             dump.dump()
-        elseif command == "refuel" then
+        elseif status == "refuel" then
             nav.goto_block(fuel.location)
             dir.face(dir.NEG_Z)
             fuel.refuel()
-        elseif "new spot" then
-            protocol.spot_request()
+        elseif status == "mine" then
+            if quarry_location == nil then
+                protocol.spot_request()
+                if quary_coroutine ~= nil then
+                    quary_coroutine = nil
+                end
+            elseif quarry_location ~= nil and quary_coroutine == nil then
+                quary_coroutine = coroutine.create(function(...)
+                    quarry.quarry_level(quarry_location.spot, quarry_location.y)
+                end)
+            elseif quary_coroutine ~= nil then
+                local status, _ = coroutine.resume(quary_coroutine)
+                if status == false then
+                    quary_coroutine = nil
+                    quarry_location = nil
+                end
+            end
         end
     end
+end
+
+local function main()
+    print("Turtle starting up! Version " .. version)
+    rednet.open("left")
+    rednet.broadcast("Turtle starting up!", "monitor")
+    parallel.waitForAll(run_mine, receive_message)
 end
 
 local function shutdown(err)
@@ -68,4 +91,4 @@ local function shutdown(err)
     rednet.close()
     nav.goto_block(location.home)
 end
-xpcall(run_mine, shutdown)
+xpcall(main, shutdown)
